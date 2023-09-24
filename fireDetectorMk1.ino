1df765e4770b65c31bdc8960e6e504d07ebd4dca
Qanita Baqibillah <114118981+qanitabaqibillah@users.noreply.github.com>
2023-09-23 21:53:13 -0400

    added Grove-LCD RGB Backlight Library for LCD
    also added minor changes to temp sensor and buzzer code

--- fireDetectorMk1.ino
+++ fireDetectorMk1.ino
@@ -1,3 +1,5 @@
+#include <rgb_lcd.h>
+
 #include <math.h>
 #if defined(ARDUINO_ARCH_AVR)
 #define debug  Serial
@@ -14,13 +16,12 @@ const int R0 = 100000;            // R0 = 100k
 const int pinTempSensor = A0;     // Grove - Temperature Sensor connect to A0
 float temp = 0;
 const int button = 2;       // connect a button
-const int buzzer = 3;       // connect a buzzer
+const int buzzer = 6;       // connect a buzzer
 void setup()
 {
-      pinMode(button, INPUT); //set button as an INPUT device
-      pinMode(buzzer, OUTPUT);   //set LED as an OUTPUT device
-
     Serial.begin(9600);
+    // pinMode(button, INPUT); //set button as an INPUT device
+    pinMode(buzzer, OUTPUT);   //set LED as an OUTPUT device
 
 }
 void loop()
@@ -36,8 +37,8 @@ void loop()
 
     float temperature = 1.0/(log(R/R0)/B+1/298.15)-273.15; // convert to temperature via datasheet
 
-    if (temperature >= 30.0) {
-        // Activate the buzzer when the temperature exceeds 30 degrees Celsius
+    if (temperature >= 15.0) {
+    //     // Activate the buzzer when the temperature exceeds 30 degrees Celsius
         digitalWrite(buzzer, HIGH);
     } else {
         // Turn off the buzzer if the temperature is below 30 degrees Celsius
