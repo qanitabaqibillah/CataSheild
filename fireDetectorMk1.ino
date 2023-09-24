adc3706561b05ac54174ed8c096224d85410711d
Qanita Baqibillah <114118981+qanitabaqibillah@users.noreply.github.com>
2023-09-24 08:00:36 -0400

    added code to activate alert message on LCD

--- fireDetectorMk1.ino
+++ fireDetectorMk1.ino
@@ -15,17 +15,32 @@ const int B = 4275;               // B value of the thermistor
 const int R0 = 100000;            // R0 = 100k
 const int pinTempSensor = A0;     // Grove - Temperature Sensor connect to A0
 float temp = 0;
-const int button = 2;       // connect a button
+const int button = 0;       // connect a button
 const int buzzer = 6;       // connect a buzzer
+
+rgb_lcd lcd;
+
+int colorR = 0;
+int colorG = 100;
+int colorB = 0;
+
 void setup()
 {
     Serial.begin(9600);
-    // pinMode(button, INPUT); //set button as an INPUT device
+    pinMode(button, INPUT); //set button as an INPUT device
     pinMode(buzzer, OUTPUT);   //set LED as an OUTPUT device
+
+    lcd.begin(16, 2);
+
+    lcd.setRGB(colorR, colorG, colorB);
 
 }
 void loop()
 {
+    // set the cursor to column 0, line 1
+    // (note: line 1 is the second row, since counting begins with 0):
+    lcd.setCursor(0, 1);
+
     int btn = digitalRead(button); //read the status of the button
     digitalWrite(buzzer, btn);
     delay(10);
@@ -37,12 +52,24 @@ void loop()
 
     float temperature = 1.0/(log(R/R0)/B+1/298.15)-273.15; // convert to temperature via datasheet
 
-    if (temperature >= 15.0) {
+    if (temperature >= 25.0) {
     //     // Activate the buzzer when the temperature exceeds 30 degrees Celsius
         digitalWrite(buzzer, HIGH);
+        colorR = 100;
+        colorG = 0;
+        colorB = 0;
+        lcd.setRGB(colorR, colorG, colorB);
+        lcd.print("WildfireDetected");
     } else {
         // Turn off the buzzer if the temperature is below 30 degrees Celsius
         digitalWrite(buzzer, LOW);
+        digitalWrite(buzzer, HIGH);
+        // print the number of seconds since reset:
+        lcd.print("Safe Environment");
+        colorR = 0;
+        colorG = 100;
+        colorB = 0;
+        lcd.setRGB(colorR, colorG, colorB);
     }
 
     Serial.print("temperature = ");
